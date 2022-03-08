864b1579e58cfbda9937b0226c199c34f18af77a
Ricardo Fulgencio Alves <ricardo.alves@socialbank.com.br>
2022-03-08 14:55:17 -0300

    Add project files.

---
+++ DevJobs.Solution/src/DevJobs.API/Entities/JobApplication.cs
@@ -0,0 +1,17 @@
+﻿namespace DevJobs.API.Entities
+{
+    public class JobApplication
+    {
+        public JobApplication(string applicantName, string applicantEmail, int idJobVacançy)
+        {
+            ApplicantName = applicantName;
+            ApplicantEmail = applicantEmail;
+            IdJobVacançy = idJobVacançy;
+        }
+
+        public int Id { get; private set; }
+        public string ApplicantName { get; private set; }
+        public string ApplicantEmail { get; private set; }
+        public int IdJobVacançy { get; private set; }
+    }
+}
